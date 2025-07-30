b6b25821f7ab9a98f481b7ba69122c4fe0be6359
cazuni13 <joao.2023014145@alunocesul.com>
2025-07-29 22:41:06 -0300

    configuração de tema

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,17 @@
+import { Flex, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import loginImage from "../../public/assets/loginImage.gif"
+
 export default function Login(){
     return(
-        <div>
-            Login
-        </div>
+      <Flex w="100vw" h="100vh">
+        <Flex w="50vw"  bg="#2c73eb" align="center" justify="center">
+          <Image w={500} h={500} src={loginImage.src}/>
+        </Flex>
+
+        <VStack w="50vw" bg="green" direction="column">
+          <Heading as="h1" >Acount Login</Heading>
+          <Text>If you are already a member you can login with your email address and password.</Text>
+        </VStack>
+      </Flex>
     )
 }
