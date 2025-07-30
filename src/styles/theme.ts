80f3e6bd6ea573d2b21f404fe171501cec905312
cazuni13 <joao.2023014145@alunocesul.com>
2025-07-29 21:51:52 -0300

    configuração de tema

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "@chakra-ui/react";
+
+export const themeConfig = defineConfig({
+  globalCss:{
+    "html, body":{
+      backgroundColor: "#ffffffff"
+    }
+  },
+  theme:{
+    tokens:{
+      colors:{}
+    }
+  }
+
+})
